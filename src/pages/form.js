2e132dad3371b68873e3a841a8d9bf1e22535368
yanochoa <yanochoa@email.arizona.edu>
2018-11-03 21:07:03 -0700

    added form styling

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import "../form.scss";
+import "../../sass/style.scss";
 export default class Form extends Component {
   render() {
     return (
@@ -10,10 +11,37 @@ export default class Form extends Component {
           data-netlify="true"
           data-netlify-bot="bot-field"
         >
-          <input name="name" placeholder="Alexo Bezos" type="text" />
-          <button type="button" className="btn btn-primary">
-            Send
-          </button>
+          <div className="inputs">
+            <div class="form-group">
+              <label for="formGroupTypeOfPropertyInputs">
+                Type of Property
+              </label>
+              <select class="form-control" id="formGroupTypeOfPropertyInput">
+                <option>Entire Place</option>
+                <option>Private Room</option>
+                <option>Shared Room</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput">Property Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="formGroupExampleInput"
+                placeholder="Example input"
+              />
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Address</label>
+              <input
+                type="text"
+                class="form-control"
+                id="formGroupExampleInput2"
+                placeholder="Another input"
+              />
+            </div>
+            <button>Send</button>
+          </div>
         </form>
       </div>
     );
